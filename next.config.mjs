ad63e0e4534b3e6b745b3ca3ff814aa52b946a99
bikram <6bikramsutar@gmail.com>
2024-08-06 21:47:48 +0530

    update output export

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: process.env.NEXT_PUBLIC_ENVIRONMENT === "production" ? "" : undefined,
+  output:
+    process.env.NEXT_PUBLIC_ENVIRONMENT === "production" ? "export" : undefined,
   api: {
     bodyParser: false,
   },
